a32d6edf052670a0fa27ded096caa64f10dfd707
DuongTai0106 <duongthanhtai0106@gmail.com>
2025-11-11 14:44:31 +0700

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
@@ -18,9 +19,17 @@ app.use(
     credentials: true,
   })
 );
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
